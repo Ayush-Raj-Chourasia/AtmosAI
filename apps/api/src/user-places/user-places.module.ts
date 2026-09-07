3802f43fbe2cb7148f5fea9af7e72bf8ecc9f0a5
Ayush Raj Chourasia <iter.student.alpha@gmail.com>
2026-09-08 04:34:51 +0530

    refactor(cleanup): remove legacy Indonesian disaster modules and scrapers from backend

--- apps/api/src/user-places/user-places.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UserPlacesController } from './user-places.controller';
-import { UserPlacesService } from './user-places.service';
-
-@Module({
-  controllers: [UserPlacesController],
-  providers: [UserPlacesService]
-})
-export class UserPlacesModule {}
